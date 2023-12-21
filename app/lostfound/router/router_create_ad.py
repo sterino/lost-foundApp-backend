3fb38bf8fd71dea2255b1660d44119b5e8108f91
Aibatyr Khassenov <107022333+sterino@users.noreply.github.com>
2023-12-21 14:57:45 +0600

    create_ad

--- app/lostfound/router/router_create_ad.py
+++ app/lostfound/router/router_create_ad.py
@@ -1,10 +1,8 @@
 from fastapi import Depends
 
-from app.utils import AppModel
-
-
 from app.auth.adapters.jwt_service import JWTData
 from app.auth.router.dependencies import parse_jwt_user_data
+from app.utils import AppModel
 
 from ..service import Service, get_service
 from . import router
@@ -21,7 +19,7 @@ class createPostResponse(AppModel):
     new_post_id: str
 
 
-@router.post("/", status_code=200)
+@router.post("/create", status_code=200)
 def create_post(
     input: createPostRequest,
     jwt_data: JWTData = Depends(parse_jwt_user_data),
