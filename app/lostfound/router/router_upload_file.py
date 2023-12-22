a0dac42d8d664b080c9dc7cfdcc0aec4263a974b
Aibatyr Khassenov <107022333+sterino@users.noreply.github.com>
2023-12-23 01:17:53 +0600

    check3

--- app/lostfound/router/router_upload_file.py
+++ app/lostfound/router/router_upload_file.py
@@ -5,22 +5,24 @@ from app.auth.router.dependencies import parse_jwt_user_data
 
 from ..service import Service, get_service
 from . import router
+import os
 
-
-@router.post("/{post_id}/media", status_code=200)
+@router.post("/{post_id}/media/", status_code=200)
 def set_ads_image(
     file: UploadFile,
     ad_id: str,
     jwt_data: JWTData = Depends(parse_jwt_user_data),
     svc: Service = Depends(get_service),
 ):
+    if file.filename is None:
+        file.filename = os.path.basename(file)
     user_id = jwt_data.user_id
 
     post = svc.repository.get_ad_by_id(ad_id)
 
     if str(post["user_id"]) != user_id:
         raise Response(status_code=404)
-    
+
     url = svc.s3_service.upload_file(file.file, file.filename)
 
     svc.repository.add_ads_media(ad_id, url)
