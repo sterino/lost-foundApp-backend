26bf0792f58b689607cd6e88845ae94ae02a61bb
Aibatyr Khassenov <107022333+sterino@users.noreply.github.com>
2023-12-23 02:50:18 +0600

    check10

--- app/lostfound/adapters/s3_service.py
+++ app/lostfound/adapters/s3_service.py
@@ -17,16 +17,15 @@ class S3Service:
 
         self.s3.upload_fileobj(file, bucket, filekey)
 
-        bucket_location = boto3.client("s3").get_bucket_location(Bucket=bucket)
-        object_url = "https: //s3-{0}.amazonaws.com/{1}/{2}".format(
-            bucket_location["LocationConstraint"], bucket, filekey
+        object_url = "https://{0}.s3.amazonaws.com/ads/{1}/{2}".format(
+            bucket, sh_id, filekey
         )
 
         return object_url
 
     def delete_file(self, file_id: str, filepath: str):
         filepath = filepath.split("/")[-1]
-        bucket = "220103289-bucket"
+        bucket = "220103289"
         filekey = f"ads/{file_id}/{filepath}"
 
         self.s3.delete_object(Bucket=bucket, Key=filekey)
